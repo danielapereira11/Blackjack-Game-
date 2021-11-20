f20db59e7ffc2b7365c682fcc7d2ea9befbf4c8d
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-20 17:05:12 +0000

    Hard coded CARDS and SUM variable values

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const cardEl = document.querySelector("#card-el");
 const sumEl = document.querySelector("#sum-el");
 const statementEl = document.querySelector("#statement-el");
 const player = document.querySelector("#player-el");
+
+let cards = [4, 6, 9];
+let sum = cards[0] + cards[1] + cards[2];
+
+cardEl.innerHTML = "Cards: " + cards + " ";
+sumEl.innerHTML = "Sum: " + sum + " ";
+// console.log(sum);
