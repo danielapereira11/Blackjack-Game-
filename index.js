401414659e8c674e00c75a985e9d5b34469c12e7
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-20 17:31:32 +0000

    correction of generateStatement function to include the value "0"

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const sumEl = document.querySelector("#sum-el");
 const statementEl = document.querySelector("#statement-el");
 const playerEl = document.querySelector("#player-el");
 
-let cards = [4, 6, 9];
-let sum = cards[0] + cards[1] + cards[2];
+let cards = [];
+let sum = "";
 
 cardEl.innerHTML = "Cards: " + cards + " ";
 sumEl.innerHTML = "Sum: " + sum + " ";
 
 function generateStatement() {
-  if (sum < 21) {
+  if (sum > 1 && sum < 21) {
     statementEl.innerHTML =
       "You are still in the game! Do you want to draw a new card?";
   } else if (sum > 21) {
     statementEl.innerHTML = "You are OUT!";
-  } else {
+  } else if (sum === 21) {
     statementEl.innerHTML = "Congratulations! You got BLACKJACK!";
+  } else {
+    statementEl.innerHTML = "Do you want to play a round?";
   }
 }
 
 generateStatement();
-
-// console.log(sum);
