13ea90724f8014b0b25057b97d654390662985c2
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-20 16:56:46 +0000

    Added player element

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const newCardBtn = document.querySelector("#new-card-btn");
 const cardEl = document.querySelector("#card-el");
 const sumEl = document.querySelector("#sum-el");
 const statementEl = document.querySelector("#statement-el");
-
+const player = document.querySelector("#player-el");
