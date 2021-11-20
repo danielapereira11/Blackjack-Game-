7f7283b4bb1f87005ada0a51d61a81151dc4b9c8
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-20 16:44:51 +0000

    JS: Created main const variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const startBtn = document.querySelector("#start-game-btn");
+const newCardBtn = document.querySelector("#new-card-btn");
+const cardEl = document.querySelector("#card-el");
+const sumEl = document.querySelector("#sum-el");
+const statementEl = document.querySelector("#statement-el");
 
