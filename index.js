4970b8225634861c2ab407cb5cd6899713172905
danielapereira11 <86188533+danielapereira11@users.noreply.github.com>
2021-11-20 17:12:58 +0000

    Added if/else generateStatment() function
    Changes the statement shown overhead with the value of the sum total (<21 , > 21, =21)

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const newCardBtn = document.querySelector("#new-card-btn");
 const cardEl = document.querySelector("#card-el");
 const sumEl = document.querySelector("#sum-el");
 const statementEl = document.querySelector("#statement-el");
-const player = document.querySelector("#player-el");
+const playerEl = document.querySelector("#player-el");
 
 let cards = [4, 6, 9];
 let sum = cards[0] + cards[1] + cards[2];
 
 cardEl.innerHTML = "Cards: " + cards + " ";
 sumEl.innerHTML = "Sum: " + sum + " ";
+
+function generateStatement() {
+  if (sum < 21) {
+    statementEl.innerHTML =
+      "You are still in the game! Do you want to draw a new card?";
+  } else if (sum > 21) {
+    statementEl.innerHTML = "You are OUT!";
+  } else {
+    statementEl.innerHTML = "Congratulations! You got BLACKJACK!";
+  }
+}
+
+generateStatement();
+
 // console.log(sum);
